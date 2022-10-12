833067942fc186d3b20cc825092ef04049b22810
Chad <chadndonahue@gmail.com>
2022-10-11 17:32:09 -0700

    added functionality for subtraction, multiplacation, and division.  Everything works now need to make better with inquirer

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ intNum2 = parseInt(num2)
 if(process.argv[2]==="add"){
     let sum = intNum1 + intNum2
     console.log(sum)
-} 
+} else if(process.argv[2]==="subtract"){
+    let sum = intNum1 - intNum2
+    console.log(sum)
+} else if(process.argv[2]==="multiply"){
+    let sum = intNum1 * intNum2
+    console.log(sum)
+} else if(process.argv[2]==="divide"){
+    let sum = intNum1 / intNum2
+    console.log(sum)
+} else{console.log("Please enter add, subtract, multiply, or divide and then your two numbers")}
